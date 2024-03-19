dcc6ff1ebca468de6f4011de099a40af3db0f4bc
Hieu Dang <hieudangk27@Hieus-MacBook-Pro.local>
2024-03-19 00:51:07 -0400

    finalize front-end with glassmorphism styling

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -50,3 +50,96 @@ export const FooterCon = styled.div`
 export const FooterLink = styled(Link)`
   color: white;
 `;
+
+export const QuoteGeneratorCon = styled.div`
+  min-height: 350px;
+  min-width: 350px;
+  height: 70vh;
+  width: 70vw;
+  border: 2px solid #ffffff;
+  border-radius: 15px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  z-index: 2;
+
+  background: rgba(0, 0, 70, 0.3);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+`;
+
+export const QuoteGeneratorInnerCon = styled.div`
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  width: 100%;
+`;
+
+export const QuoteGeneratorTitle = styled.div`
+  font-family: 'Permanent Marker', cursive;
+  font-size: 50px;
+  text-align: center;
+  color: white;
+  padding: 0px 20px 0px 20px;
+  position: relative;
+  @media only screen and (max-width: 600px){
+    font-size: 30px;
+  }
+`;
+
+export const QuoteGeneratorSubtitle = styled.div`
+  color: white;
+  font-family: 'Caveat', cursive;
+  font-size: 35px;
+  position: relative;
+  width: 100%;
+  text-align: center;
+  padding: 0px 20px 0px 20px;
+  @media only screen and (max-width: 600px){
+    font-size: 25px;
+  }
+`;
+
+export const GenerateQuoteButton = styled.div`
+  height: 100px;
+  width: 300px;
+  border: 2px solid darkgrey;
+  border-radius: 20px;
+
+  margin-top: 20px;
+  position: relative;
+  top: 20px;
+  margin: auto;
+  cursor: pointer;
+  transition: 0.2s all ease-in-out;
+  transform-origin: center;
+
+  background: rgba(0, 0, 70, 0.3);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+
+  &:hover{
+    filter: brightness(3);
+    transition: 0.2s all ease-in-out;
+    transform: scale(1.1);
+    transform-origin: center;
+  }
+`;
+
+export const GenerateQuoteButtonText = styled.div`
+  color: white;
+  font-family: 'Caveat', cursive;
+  font-size: 35px;
+  top: 20%;
+  position: absolute;
+  width: 100%;
+  text-align: center;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import styles from "@/styles/Home.module.css";
 
 
 // components
-import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GradientBackgroundCon, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorTitle, QuoteGeneratorSubtitle, GenerateQuoteButton, GenerateQuoteButtonText } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 
 // assets
 import LeftCloud from '../assets/cloud-and-thunder.png';
@@ -33,6 +33,7 @@ export default function Home() {
           alt="cloud-background1"
         />
 
+        {/* Background images */}
         <BackgroundImage2
           src={RightCloud}
           height="300"
@@ -40,12 +41,36 @@ export default function Home() {
         />
       </GradientBackgroundCon>
 
+      {/* Quote Generator Modal */}
+
+      {/* Quote Generator */ }
+      <QuoteGeneratorCon>
+        <QuoteGeneratorInnerCon>
+          <QuoteGeneratorTitle>
+            Daily Inspiration Generator
+          </QuoteGeneratorTitle>
+          <QuoteGeneratorSubtitle>
+            Looking for a splash of inspiration?
+            Generate a quote card with a random
+            inspirational quote provided by <FooterLink href="https://zenquotes.io/" target="_blank">ZenQuotes Api</FooterLink>
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>
+                Make a Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorSubtitle>
+        </QuoteGeneratorInnerCon>
+      </QuoteGeneratorCon>
+
+
+      
+
+      {/* Footer */}
       <FooterCon>
         <>
           Quotes generated: {numQuotes}
           <br/>
-          Developed by Hieu Dang
-          <FooterLink href=""></FooterLink>
+          Developed by <FooterLink href="https://github.com/dhieu24/quote-generator">@Hieu Dang</FooterLink>
         </>
       </FooterCon>
       
